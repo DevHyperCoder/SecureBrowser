fa7208d7603c3fad67c06fd44a4d35fae7bf7dfe
DevHyperCoder <arduinoleo88@gmail.com>
2021-04-08 14:03:51 +0530

    Privacy guard feature using security flags for host activity

--- app/src/main/java/com/devhypercoder/securebrowser/MainActivity.kt
+++ app/src/main/java/com/devhypercoder/securebrowser/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.devhypercoder.securebrowser
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Remove screenshots and prevent from appearing in Recent App list
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+
         setContentView(R.layout.main_activity)
     }
 }
